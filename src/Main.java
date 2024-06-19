632c8f6a9b20e3ee6929a5fa70648959a390797f
gleb_papulovsky <papulovskijglech@gmail.com>
2024-06-19 17:14:02 +0300

    It's works

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
         int[] prices = { 13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30 };
 
-        System.out.println("Для 31: " + countMore(prices, 31)); // 0
+        
         System.out.println("Для 26: " + countMore(prices, 26)); // 2
         System.out.println("Для 25: " + countMore(prices, 25)); // 2
         System.out.println("Для 20: " + countMore(prices, 20)); // 8
+        System.out.println("Для 31: " + countMore(prices, 31)); // 0
     }
 
     public static int countMore(int[] prices, int money) {
@@ -25,8 +26,22 @@ public class Main {
             
             // Ваш код:
             // Если в middle первый недоступный товар, вернуть размер массива минус middle
+            if (prices[middle]>money && prices[middle-1 ]<money){
+                return prices.length - middle;
+            }
+            else if (prices[middle]<=money && prices[middle+1 ]>money){
+                return prices.length - middle-1;
+
+            }
             // Если в middle доступный товар, то искать нужно правее - left = middle + 1
+            else if(prices[middle]<=money){
+                left = middle + 1;
+
+            }
             // Если в middle недоступный товар, то искать нужно левее - right = middle - 1
+            else if (prices[middle]>money){
+                right = middle - 1;
+            }
             
         }
         return 0; // иначе не скомпилится, джава сама не знает бинпоиск
